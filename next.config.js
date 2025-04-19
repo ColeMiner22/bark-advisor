a5fdfafaa5c040b0bb350d3ffecfa70f3cc0105c
ColeMiner22 <sullivancole12@gmail.com>
2025-04-19 17:25:45 -0400

    Update deployment configurations for Vercel

--- next.config.js
+++ next.config.js
@@ -5,7 +5,11 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_OPENAI_API_KEY: process.env.OPENAI_API_KEY,
     AMAZON_AFFILIATE_TAG: process.env.AMAZON_AFFILIATE_TAG
-  }
+  },
+  poweredByHeader: false,
+  generateEtags: false,
+  distDir: '.next',
+  cleanDistDir: true
 }
 
 module.exports = nextConfig 
