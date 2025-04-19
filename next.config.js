f2340854a4b4217cd77517b3705b3c62afa24c23
ColeMiner22 <sullivancole12@gmail.com>
2025-04-19 19:41:26 -0400

    Initial commit

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   env: {
-    NEXT_PUBLIC_OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
     AMAZON_AFFILIATE_TAG: process.env.AMAZON_AFFILIATE_TAG
   },
   poweredByHeader: false,
